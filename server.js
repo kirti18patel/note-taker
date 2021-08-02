89cb8e5db483e014dad749c36b7dace029f61145
kirti18patel <kirti18patel@yahoo.com>
2021-08-02 15:29:06 -0400

    html routes created successfully

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 
@@ -6,12 +7,12 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {

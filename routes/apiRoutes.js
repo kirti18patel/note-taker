a7420ae949c09301e00a8972005f4c43b5ad4d4d
kirti18patel <kirti18patel@yahoo.com>
2021-08-02 15:46:46 -0400

    put notes api added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
   const notes = JSON.parse(
@@ -8,5 +9,20 @@ router.get('/notes', (req, res) => {
   );
   res.json(notes);
 });
+
+router.post('/notes', (req, res) => {
+    const notes = JSON.parse(
+      fs.readFileSync(path.join(__dirname, '../db/db.json'))
+    );
+    const newNote = req.body;
+  
+    newNote.id = uuidv4();
+    notes.push(newNote);
+    fs.writeFileSync(
+      path.join(__dirname, '../db/db.json'),
+      JSON.stringify(notes)
+    );
+    res.send('note added');
+  });
 
 module.exports = router;

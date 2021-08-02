f36ff5f5fc36a038e30050d395afa9a5cdb29f0b
kirti18patel <kirti18patel@yahoo.com>
2021-08-02 15:35:58 -0400

    get notes api added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,12 @@
-const path = require('path');
 const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+router.get('/notes', (req, res) => {
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../db/db.json'))
+  );
+  res.json(notes);
+});
 
 module.exports = router;
